d79ad995f6dcfb946c144d325c39507e46a77e8f
Liam <58812167+LiamTurnerAU@users.noreply.github.com>
2019-12-13 01:18:09 +1030

    ft-PlayerJump
    -Added player jumping and floor collision

---
+++ MK Runner/Assets/Scripts/Management/GameEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvents
+{
+    //Handles ingame events
+
+    public delegate void voidDelegate();
+
+    public static event voidDelegate OnGameEnd;
+    public static void InvokeGameEnd()
+    {
+        OnGameEnd.Invoke();
+    }
+
+}
---
+++ MK Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Controls the player
+    public float playerSpeed, playerJump;
+    public LayerMask groundLayer;
+
+    public bool canJump = true;
+    private Rigidbody2D playerRB;
+    private Collider2D playerCollider;
+
+    
+
+    private void Start()
+    {
+        playerCollider = GetComponent<Collider2D>();
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        /*Touch Controls
+         *
+         * if (Input.touchCount > 0)
+         {
+             Jump();
+
+
+         }*/
+
+
+        if (Physics2D.IsTouchingLayers(playerCollider, groundLayer))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+
+        }
+
+        if (Input.GetMouseButtonDown(0) && canJump )
+        {
+            Jump();
+
+        }
+    }
+
+    
+
+    private void Jump()
+    {
+        playerRB.velocity += new Vector2(0, playerJump);
+    }
+}
